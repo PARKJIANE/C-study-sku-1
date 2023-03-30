7a5c71ef31994a367a5ff17164a26fd7e80b3a1a
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2023-03-30 19:08:24 +0900

    Rename 2023.03.28 - one.cpp to 2023.03.28 - one.c

---
+++ 2023.03.28 - one.c
@@ -0,0 +1,63 @@
+/* 
+#include <stdio.h>
+int main(void)
+
+{
+	printf("마트에서 사용 품목\n");
+	printf("==================\n");
+	printf("사과, 우유, 빵\n");
+	printf("==================\n");
+	return 0;
+}
+*/
+
+/*
+#include <stdio.h>
+int main(void)
+{
+	printf("안녕하세요 ? \n");
+	printf("이번 코드에는 많은 오류가 있다네요\n");
+	printf("제가 다 고쳐보겠습니다.\n");
+	return 0;
+}
+*/
+
+//덧셈 프로그램
+/*
+#include <stdio.h>
+int main(void)
+{
+	int x;
+	int y;
+	int sum;
+
+	x = 100;
+	y = 200;
+	sum = x + y;
+
+	printf("두수의 합 : %d", sum);
+	return 0;
+}
+*/
+
+//두개의 숫자의 합을 계산하는 프로그램
+/*
+#include <stdio.h>
+
+int main() {
+	int x = 20, y = 10;
+	int sum, diff, mul, div;
+
+	sum = x + y;
+	diff = x - y;
+	mul = x * y;
+	div = x / y;
+
+	printf("두 수의 합 : %d\n", sum);
+	printf("두 수의 차 : %d\n", diff);
+	printf("두 수의 곱 : %d\n", mul);
+	printf("두 수의 몫 : %d\n", div);
+	printf("두 수의 나머지 : %d", x % y);
+	return 0;
+}
+*/
