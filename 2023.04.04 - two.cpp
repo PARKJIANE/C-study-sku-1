89ade13835176b57d7390befacb59ab9bc947793
PARKJIANE <102977286+PARKJIANE@users.noreply.github.com>
2023-04-04 16:11:10 +0900

    Add files via upload

---
+++ 2023.04.04 - two.cpp
@@ -0,0 +1,42 @@
+// 평균 계산하기 프로그램
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	double num1, num2, num3;
+	double sum, avg;
+
+	printf("3개의 실수를 입력하시오: ");
+	scanf("%lf %lf %lf", &num1, &num2, &num3);
+
+	sum = num1 + num2 + num3;
+	avg = sum / 3;
+	printf("합계=%.2f\n", sum); //.2f 소수점 2번째 까지 표시
+	printf("평균=%.2f\n", avg);
+
+	return 0;
+}
+*/
+
+//사각형의 둘레와 면적
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	double w;
+	double h;
+	double area;
+	double perimeter;
+	w = 10.0;
+	h = 5.0;
+	area = w * h;
+	perimeter = 2 * (w + h);
+
+	printf("사각형의 넓이: %lf\n", area);
+	printf("사각형의 둘레: %lf\n", perimeter);
+
+	return 0;
+}
+*/
